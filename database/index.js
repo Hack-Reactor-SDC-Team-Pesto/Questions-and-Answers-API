435148f95a7ed0c5e8878eb271c945295b9507db
Cheryl Casas <cherylccasas@gmail.com>
2022-03-08 22:20:14 -0700

    Created routes and cyphers

--- database/index.js
+++ database/index.js
@@ -1,23 +1,107 @@
 const neo4j = require('neo4j-driver')
+// import neo4j from 'neo4j-driver'
 
-const driver = neo4j.driver('bolt://localhost:7687', neo4j.auth.basic('Neo4J', 'G3niusch335e'))
-const session = driver.session()
+const driver = neo4j.driver('bolt://localhost:7687', neo4j.auth.basic('neo4j', 'G3niusch335e'))
 
-console.log('TEST', {driver: session})
+// console.log('TEST', {driver: session})
 
+driver.verifyConnectivity()
 
-const getAllQ = async (product_id) => {
-  const session = this.driver.session()
-  const res = await session.getAllQ(tx => {
+const getAllq = async (product_id) => {
+  const session = driver.session()
+  const res = await session.readTransaction(tx => {
     return tx.run(
-      `MATCH (:Product {product_id: ${product_id})--(q:Question)
+      `MATCH (:Product {product_id: ${product_id}})--(q:Question)
       RETURN q LIMIT 500`
+    )
+  })
+  await session.close()
+  return res.records
+}
+
+const getAlla = async (question_id) => {
+  const session = driver.session()
+  const res = await session.readTransaction(tx => {
+    return tx.run(
+      `MATCH (:Question {question_id: ${question_id}})--(a:Answer)
+      RETURN a`
+    )
+  })
+  await session.close()
+  return res
+}
+
+const reportQ = async (question_Id) => {
+  const session = driver.session()
+  const res = await session.writeTransation(tx => {
+    return tx.run(
+      `match (q:Question {question_id: ${question_id}) set q.reported = true`
     )
   })
   await session.close()
 }
 
+const reportA = async (answer_Id) => {
+  const session = driver.session()
+  const res = await session.writeTransation(tx => {
+    return tx.run(
+      `match (a:Answer {answer_id: ${answer_id}) set a.reported = true`
+    )
+  })
+  await session.close()
+}
 
-module.exports = {getAllQ}
+const helpfulQ = async (question_id) => {
+  const session = driver.session()
+  const res = await session.writeTransation(tx => {
+    return tx.run(
+      `match (q:Question {question_id: ${question_id}) set q.question_helpfulness = +1`
+    )
+  })
+  await session.close()
+}
+
+const helpfulA = async (answer_id) => {
+  const session = driver.session()
+  const res = await session.writeTransation(tx => {
+    return tx.run(
+      `match (a:Answer {answer_id: ${answer_id}) set a.helpfulness = +1`
+    )
+  })
+  await session.close()
+}
+
+const addQ = async (question_body, asker_name, asker_email, product_id) => {
+  const session = driver.session()
+  const res = await session.writeTransation(tx => {
+    return tx.run(
+      `match (p:Product {product_id: ${product_id}) merge (q:Question {question_id : count(q)+1}) set q.question_body = ${question_body}, q.asker_name = ${asker_name}, q.asker_email = ${asker_email}`
+    )
+  })
+  await session.close()
+}
+
+const addA = async (question_id, answerer_name, answerer_email, body) => {
+  const session = driver.session()
+  const res = await session.writeTransation(tx => {
+    return tx.run(
+      `match (q:Question {question_id: ${question_id}) merge (a:Answer {answer_id : count(a)+1}) set a.body = ${body}, a.answerer_name = ${answerer_name}, a.answerer_email = ${answerer_email}`
+    )
+  })
+  await session.close()
+}
+
+// console.log('THIS IS getALLQ---->', typeof getAllQ)
+
+module.exports = {
+  getAllq,
+  getAlla,
+  reportQ,
+  reportA,
+  helpfulQ,
+  helpfulA,
+  addA,
+  addQ
+}
 
 
--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const axios = require('axios');
 const path = require('path');
-const {} = require ("../database/index.js")
+const {getAllq, getAlla, reportQ, reportA, helpfulQ, helpfulA} = require ("../database/index.js")
 
 const PORT = 3000;
 const app = express();
@@ -9,10 +9,256 @@ const app = express();
 // app.use(express.static(path.join(__dirname, '..', 'public')));
 app.use(express.json())
 
-app.get('/{product}', (req, res) => {
-
-  console.log('TEST MET')
+app.get('/qa/questions/', async(req, res) => {
+  // console.log ('-----PARAMS-----', req)
+  let product_id = req.query.product_id
+  // console.log(getAllQ(product_id))
+  try {
+    const questions = await getAllq(product_id)
+    res.status(200).send(questions)
+  } catch(err){
+    res.status(400).send(err)
+  }
+  // console.log('QUESTIONS FROM SERVER', questions)
 });
+
+app.get('/qa/questions/:question_id/answers', async(req, res) => {
+  console.log ('-----PARAMS-----', req)
+  let question_id = req.params.question_id
+
+  try {
+    const answers = await getAlla(question_id)
+    res.status(200).send(answers)
+  } catch(err){
+    console.log('err', err)
+    res.status(400).send(err)
+  }
+})
+
+app.put('/qa/questions/:question_id/report', async (req, res) => {
+  let question_id = req.params.question_id
+
+  try {
+    const reportedQ = await reportQ(question_id)
+    res.status(204).send('Successfully Reported Question')
+  } catch(err){
+    res.status(404).send('Error Reporting Question')
+  }
+})
+
+//! Report Question
+
+// app.put('/qa/questions/:question_id/report', (req, res) => {
+//   console.log('report question!!!!!!!!!!')
+//   api.reportQuestion(req.params)
+//     .then((success) => {
+//       res.status(204);
+//       res.send('Successfully Reported Question');
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//       res.status(404);
+//       res.send('Error Reporting Question');
+//     })
+// })
+
+// const reportQuestion = (questionId) => {
+//   return axios({
+//     method: 'PUT',
+//     url: `${url}/qa/questions/${questionId.question_id}/report`,
+//     headers: {
+//       'Authorization': `${TOKEN}`
+//     }
+//   })
+// }
+
+app.put('/qa/answers/:answer_id/report', async (req, res) => {
+  let answer_id = req.params.answer_id
+
+  try{
+    const reportedA = await reportA (()=>{
+      res.status(204).send('Successfully Reported Answer')
+    })
+  } catch(err){
+    res.status(404).send('Error Reporting Answer')
+  }
+})
+
+//! Report Answer
+
+// app.put('/qa/answers/:answer_id/report', (req, res) => {
+//   api.reportAnswer(req.params)
+//     .then((success) => {
+//       res.status(204);
+//       res.send('Successfully Reported Answer');
+//     })
+//     .catch((err) => {
+//       res.status(404);
+//       res.send('Error Reporting Answer');
+//     })
+// })
+
+// const reportAnswer = (answerId) => {
+//   return axios({
+//     method: 'PUT',
+//     url: `${url}/qa/answers/${answerId.answer_id}/report`,
+//     headers: {
+//       'Authorization': `${TOKEN}`
+//     }
+//   })
+// }
+
+app.post('/qa/questions', async(req, res) => {
+  let question_body = req.body.question_body;
+  let asker_name = req.body.asker_name;
+  let asker_email = req.body.asker_email
+  let product_id = req.query.product_id
+
+  try {
+    const addQuestion = await addQ ((question_body, asker_name, asker_email, product_id))
+    res.status(201).send('Successfully Posted A Question')
+  } catch (err) {
+    res.status(404).send('Error Posting A Question')
+  }
+})
+
+//! Add a question
+
+// app.post('/qa/questions', (req, res) => {
+//   api.addQuestion(req.body)
+//     .then((success) => {
+//       res.status(201);
+//       res.send('Successfully Posted A Question');
+//     })
+//     .catch((err) => {
+//       //console.log('Error Posting Question to API', err);
+//       res.status(404);
+//       res.send('Error Posting A Question');
+//     })
+// })
+
+// const addQuestion = (data) => {
+//   let options = {
+//     url: `${url}/qa/questions`,
+//     headers: {
+//       'Authorization': `${TOKEN}`,
+//       'Content-Type': 'application/json'
+//     },
+//   };
+//   return axios.post(options.url, data, options)
+// }
+
+app.post('/qa/questions', async(req, res) => {
+  let question_id = req.params.question_id;
+  let answerer_name = req.body.answerer_name;
+  let answerer_email = req.body.answerer_email
+  let body = req.query.body
+
+  try {
+    const addQuestion = await addQ ((question_body, asker_name, asker_email, product_id))
+    res.status(201).send('Successfully Posted an Answer')
+  } catch (err) {
+    res.status(404).send('Error Posting an Answer')
+  }
+})
+//! Add an answer
+
+// app.post('/qa/questions/:question_id/answers', (req, res) => {
+//   api.addAnswer(req.params, req.body)
+//     .then((success) => {
+//       res.status(201);
+//       res.send('Successfully Posted an Answer');
+//     })
+//     .catch((err) => {
+//       res.status(404);
+//       res.send('Error Posting an Answer');
+//     })
+// })
+
+// const addAnswer = (questionId, data) => {
+
+//   let options = {
+//     url: `${url}/qa/questions/${questionId.question_id}/answers`,
+//     headers: {
+//       'Authorization': `${TOKEN}`,
+//       'Content-Type': 'application/json'
+//     },
+//   };
+//   return axios.post(options.url, data, options)
+// }
+
+app.put('/qa/question/:question_id/helpful', async(req, res) => {
+  let question_id = req.params.question_id
+
+  try {
+    const helpfulQuestion = helpfulQ ((question_id) => {
+      res.status(204).send('Successfully Marked Question Helpful')
+    })
+  } catch (err) {
+    res.status(404).send('Error Marking Question Helpful')
+  }
+})
+//! Mark Question as Helpful
+
+// app.put('/qa/questions/:question_id/helpful', (req, res) => {
+//   api.markQHelpful(req.params)
+//     .then((success) => {
+//       res.status(204);
+//       res.send('Successfully Marked Question Helpful');
+//     })
+//     .catch((err) => {
+//       res.status(404);
+//       res.send('Error Marking Question Helpful');
+//     })
+// })
+
+// const markQHelpful = (questionId) => {
+//   return axios({
+//     method: 'PUT',
+//     url: `${url}/qa/questions/${questionId.question_id}/helpful`,
+//     headers: {
+//       'Authorization': `${TOKEN}`,
+//       'Content-Type': 'application/json'
+//     }
+//   })
+// }
+
+app.put('/qa/answers/:answer_id/helpful', async (req, res) => {
+  let answer_id = req.params.answer_id
+  try {
+    const helpfulAnswer = helpfulA((answer_id) => {
+      res.status(204).send('Successfully Marked Answer Helpful')
+    })
+  } catch {
+    res.status(404).send('Error Marking Answer Helpful')
+  }
+})
+
+//! Mark Answer as Helpful
+
+// app.put('/qa/answers/:answer_id/helpful', (req, res) => {
+
+//   api.markAHelpful(req.params)
+//     .then((success) => {
+//       res.status(204);
+//       res.send('Successfully Marked Answer Helpful');
+//     })
+//     .catch((err) => {
+//       res.status(404);
+//       res.send('Error Marking Answer Helpful');
+//     })
+// })
+
+// const markAHelpful = (answerId) => {
+//    return axios({
+//     method: 'PUT',
+//     url: `${url}/qa/answers/${answerId.answer_id}/helpful`,
+//     headers: {
+//       'Authorization': `${TOKEN}`
+//     }
+//   })
+// }
+
 
 
 app.listen(PORT, () => {
