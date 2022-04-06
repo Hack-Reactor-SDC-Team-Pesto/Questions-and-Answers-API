3f4ca8c048301746324c6107b79ddc7624fd670c
Cheryl Casas <Cherylccasas@gmail.com>
2022-04-05 21:13:56 -0600

    Removed client files

--- client/component/widgets/overview/components/little components/MainCarousel.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-const MainCarousel = ({mainImg, setMainImg, allPics}) => (
-    <img className="mainimg main-carousel-item" src={mainImg} alt="current product photo"/>
-    
-)
-
-export default MainCarousel
